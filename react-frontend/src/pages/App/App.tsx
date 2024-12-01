ed71ab7d956cacdb454e6c8d8105eb85541693e9
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-12-01 18:15:45 +0100

    feat(Front signup): HTML of the signup page
    I created the HTML of the signup page and insert the page in the routing

--- react-frontend/src/pages/App/App.tsx
+++ react-frontend/src/pages/App/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../Login/login";
+import Signup from '../Signup/signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
           {/* <Route path="*" element={<NoPage />} /> TODO: Error 404 */}
       </Routes>
     </BrowserRouter>
