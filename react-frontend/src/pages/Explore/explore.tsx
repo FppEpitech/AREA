0819dcfa6cf16c9c1ba3a7c1b1f9f3adb27baeb0
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-12-02 13:35:08 +0100

    feat(Front Explore page): Create HTML of the Explore page
    I adding the strict minimum of the explore page in HTML, there are two list to fill with the db to have infos to display

---
+++ react-frontend/src/pages/Explore/explore.tsx
@@ -0,0 +1,61 @@
+
+interface IService {
+    name: string;
+    logo: string;
+}
+
+interface IPlumCard {
+    logo1: string;
+    logo2: string;
+    service1: string;
+    service2: string;
+    description: string;
+    numberUsers: number;
+    isActivated: boolean;
+}
+
+function PlumCard (card: IPlumCard) {
+    return(
+        <div>
+            <img src={ card.logo1 } alt={ card.service1 } ></img>
+            <img src={ card.logo2 } alt={ card.service2 } ></img>
+            <p>{ card.description }</p>
+            <p>{ card.numberUsers }</p>
+        </div>
+    );
+}
+
+function ServiceCard (service : IService) {
+    return(
+        <div>
+            <img src={ service.logo } alt={ service.name } ></img>
+            <p>{ service.name }</p>
+        </div>
+    );
+}
+
+export default function Explore () {
+
+    let plumCards: IPlumCard[] = []; // TODO: Fill this list with Plum cards of the db
+    let ServiceCards: IService[] = []; // TODO: Fill this list with Service cards of the db
+
+    return (
+        <div>
+            <h1>Explore</h1>
+            <button>All</button>
+            <button>Plums</button>
+            <button>Services</button>
+            <input type="search" placeholder="Search Plums or services"></input>
+            {
+                ServiceCards.map((card, index) => (
+                    <ServiceCard key={index} {...card} />
+                ))
+            }
+            {
+                plumCards.map((card, index) => (
+                    <PlumCard key={index} {...card} />
+                ))
+            }
+        </div>
+    );
+}
