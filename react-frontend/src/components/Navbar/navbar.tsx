35f1876a2cbc09f810c3981253ac3f7b4728e32d
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-12-02 17:53:22 +0100

    feat(Navbar CSS): Create the round green background bar for the navbar
    This style is the background of the navbar

--- react-frontend/src/components/Navbar/navbar.tsx
+++ react-frontend/src/components/Navbar/navbar.tsx
@@ -3,12 +3,17 @@ import logo from '../../assets/logo58.png'
 
 export default function Navbar() {
     return (
-        <div>
-            <img src={logo} alt="Plumpy logo"></img>
-            <button type="button">Explore</button>
-            <button type="button">My Plums</button>
-            <button type="button">Create</button>
-            <button type="button">User</button>
+        // <div>
+        //     <img src={logo} alt="Plumpy logo"></img>
+        //     <button type="button">Explore</button>
+        //     <button type="button">My Plums</button>
+        //     <button type="button">Create</button>
+        //     <button type="button">User</button>
+        // </div>
+        <div className="flex justify-center min-h-screen bg-customLightBlue py-2.5">
+            <div className="flex flex-col md:flex-row bg-customGreen shadow-lg rounded-[30px] overflow-hidden w-[1797px] h-[74px]">
+
+            </div>
         </div>
     );
 }
