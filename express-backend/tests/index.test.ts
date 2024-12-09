b53ec48c316ede6eb2cd91871e2ec3e0d55f93c9
Théophile JR <theophilejeromerocher44@gmail.com>
2024-12-09 13:41:04 +0100

    feat(Backend): Test generateToken()

--- express-backend/tests/index.test.ts
+++ express-backend/tests/index.test.ts
@@ -1,7 +1,13 @@
 import { describe, it, expect } from 'vitest';
+import { generateToken} from '../routes/account';
 
 describe('main test', () => {
     it('should pass', () => {
         expect(true).toBe(true);
     });
+});
+
+it('Should generate token', () => {
+    let token = generateToken(1);
+    expect(token).not.toBe(null);
 });
