c78e01c83daa00336b36e08fe5c25624e9157d35
Théophile JR <theophilejeromerocher44@gmail.com>
2024-12-09 19:04:03 +0100

    feat(Backend): /plums get -> send the user's plums

--- express-backend/routes/plums.ts
+++ express-backend/routes/plums.ts
@@ -1,6 +1,7 @@
 import prisma from '../prismaClient'
 import express, {Request, Response} from 'express';
 import jwt from 'jsonwebtoken'
+import authenticateToken from '../middlewares/isLoggedIn';
 
 const router = express.Router();
 
@@ -77,5 +78,28 @@ router.post('/', async (req: Request, res: Response) : Promise<any> => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+
+router.get('/', authenticateToken,  async (req : Request, res : Response)  => {
+    const userId = (req as any).middlewareId;
+    const plums = await prisma.plum.findMany({
+        where: { userId },
+        include: {
+            action: {
+                include: { actionTemplate: true }
+            },
+            trigger: {
+                include: { triggerTemplate: true }
+            }
+        }
+    }).catch((err : any) => {
+        console.log(err);
+        res.status(500).json({error: 'Internal server error' });
+        return null;
+    });
+    if (plums) {
++       res.status(200).json(plums);
+    }
+})
 
 export default router;
