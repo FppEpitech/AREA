a44fbab8904d39e3f9ce8200e1821a90b5ebac93
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-12-22 16:37:56 +0100

    feat(MyPlums): Create CSS of the page
    The MyPlums page has now a table with elements to displayed, but for the moment the link isn't made with the back

--- react-frontend/src/pages/MyPlums/myPlums.tsx
+++ react-frontend/src/pages/MyPlums/myPlums.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-// import Navbar from "../../components/Navbar/navbar";
+import Navbar from "../../components/Navbar/navbar";
+import feather from '../../assets/myPlums/feather.svg';
 
 export default function MyPlums() {
 
-    const [myPlums, setMyPlums] = useState(["Default"]);
+    const [myPlums, setMyPlums] = useState([]);
 
     const sendLogin = async() => {
         // TODO : await setMyPlums(getMyPlums());
@@ -12,17 +13,70 @@ export default function MyPlums() {
 
     return(
         <div>
-            {/* <Navbar></Navbar> TODO : decomment this part when CSS done */}
-            <h1>My Plums</h1>
-            <input type="search" placeholder="Filter"></input>
-            {
-                myPlums.map((card, index) => (
-                    <div>
-                        <h2>{index + 1}</h2>
-                        <input type="area" disabled value={card}></input>
+            <Navbar></Navbar>
+            <div className="mt-36 w-5/6 mx-auto">
+                <h1 className="text-3xl font-bold mb-6">My Plums</h1>
+                <div className="flex justify-start mb-6">
+                    <div className="relative">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input
+                            type="search"
+                            id="search"
+                            placeholder="Search"
+                            className="block w-full p-4 ps-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-customGreen focus:border-transparent"
+                        />
                     </div>
-                ))
-            }
+                    <button
+                        className="bg-customGreen hover:bg-customDarkGreen text-white font-bold m-2 py-2 px-4 rounded-lg">
+                        Filter
+                    </button>
+                    <button
+                        className="bg-customGreen hover:bg-customDarkGreen text-white font-bold m-2 py-2 px-4 rounded-lg">
+                        Create
+                    </button>
+                </div>
+                <div className="flex justify-center">
+                    <div className="relative overflow-x-auto w-full">
+                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3 rounded-s-lg">
+                                        Name
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Apps
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 rounded-e-lg">
+                                        Status
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {myPlums.map((plum, index) => (
+                                    <tr key={index} className="bg-white dark:bg-gray-800 border-b" onClick={() => {}}>
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <div className="flex justify-start">
+                                                <img className='w-5 mx-3' src={feather} alt="Feather"></img>
+                                                {/* {plum.name} */}
+                                            </div>
+                                        </th>
+                                        {/* <td className="px-6 py-4">
+                                            {plum.services}
+                                        </td> */}
+                                        {/* <td className="px-6 py-4">
+                                            {plum.isActivated}
+                                        </td> */}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
