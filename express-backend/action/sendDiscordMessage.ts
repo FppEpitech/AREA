509d7c02a6565bb13edeea3c9255e7f910d179b9
PerAlban <alban.peralta@epitech.eu>
2025-01-07 16:10:32 +0100

    refactor(template): Change template variable to be correct with the new system
    Add .value

--- express-backend/action/sendDiscordMessage.ts
+++ express-backend/action/sendDiscordMessage.ts
@@ -2,16 +2,12 @@ import axios from 'axios';
 
 async function sendDiscordMessage(userId: number, value_json: string) {
     try {
-        console.log('Enter in sendDiscordMessage');
-        const fixedActionValue = value_json.replace(/(\w+): /g, '"$1":').replace(/'/g, '"');
-        const { webhookUrl, content } = JSON.parse(fixedActionValue);
-        await axios.post(webhookUrl, {
-            content: content,
+        const { webhookUrl, content } = JSON.parse(value_json);
+        await axios.post(webhookUrl?.value, {
+            content: content?.value,
             username: 'Plumpy',
         });
-        console.log('Message successfully sent');
     } catch (error) {
-        console.error('Error sending message:', error);
     }
 }
 
