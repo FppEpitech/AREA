06fecdb53c4656fb4f7c62f68db6f5b2dd5df315
Mathieu Robert <mathieu1.robert@epitech.eu>
2025-01-13 17:24:02 +0100

    feat(Explore page): Change search bar look
    I changed the search bar look like the one in other pages

--- react-frontend/src/components/Explore/ExploreNavbar.tsx
+++ react-frontend/src/components/Explore/ExploreNavbar.tsx
@@ -38,8 +38,9 @@ return (
       </div>
       <input
         type="search"
+        id="search"
         placeholder="Search Plums or services"
-        className="border border-gray-300 rounded-lg px-4 py-2 w-full max-w-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-customGreen"
+        className="mt-1 block w-full max-w-md px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-customGreen focus:border-customGreen"
       />
     </div>
   );
